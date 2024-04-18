8e26095bf3c5db3169beaaf92e7db79fcba2ec5e
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-18 19:40:35 +0200

    theme name fixed

--- app/src/main/java/com/example/carlydemo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/carlydemo/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun JetpackComposePlaygroundTheme(
+fun CarlyDemoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
