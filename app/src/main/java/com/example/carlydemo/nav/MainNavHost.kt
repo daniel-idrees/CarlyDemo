15b46b7453b3f91a4ed025983ebb4d8ac2e5eb2c
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-19 02:24:20 +0200

    navigation controller host configuration

---
+++ app/src/main/java/com/example/carlydemo/nav/MainNavHost.kt
@@ -0,0 +1,33 @@
+package com.example.carlydemo.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.carlydemo.carlist.CarListScreen
+import com.example.carlydemo.carselection.CarSelectionScreen
+import com.example.carlydemo.dashboard.DashboardScreen
+
+@Composable
+fun MainNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = DashboardScreen.route,
+    ) {
+        dashboardScreen {
+            DashboardScreen(
+                navigateToCarSelection = navController::navigateToCarSelection,
+                navigateToCarList = navController::navigateToCarList
+            )
+        }
+
+        carSelectionScreen {
+            CarSelectionScreen()
+        }
+
+        carListScreen {
+            CarListScreen()
+        }
+    }
+}
