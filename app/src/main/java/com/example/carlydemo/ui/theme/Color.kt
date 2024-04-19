f2d20ee54d12e8eb436cb354ab7dac921ca26f7c
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-19 02:22:11 +0200

    colors added

--- app/src/main/java/com/example/carlydemo/ui/theme/Color.kt
+++ app/src/main/java/com/example/carlydemo/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundDark = Color(0xFF23262B)
+val BackgroundLight = Color(0xFF3B404A)
+val FontLight = Color(0xFFF7F7F7)
+val FontDark = Color(0xFFC2CDDF)
+val primaryColor = Color(0xFFFB9B0A)
+val SecondaryColor = Color(0xFFFFD600)
+
