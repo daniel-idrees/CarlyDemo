7549ed61cd6300229bebcdf3d5ece7b4f0941dd2
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-19 02:22:34 +0200

    theme background modified with custom color

--- app/src/main/java/com/example/carlydemo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/carlydemo/ui/theme/Theme.kt
@@ -18,16 +18,18 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = BackgroundDark,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = BackgroundLight,
 
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
