ecc76f496560318a4b359c4ca58b49de95726495
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-19 02:24:34 +0200

    car list and car selection empty screens

---
+++ app/src/main/java/com/example/carlydemo/carlist/CarListScreen.kt
@@ -0,0 +1,7 @@
+package com.example.carlydemo.carlist
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CarListScreen() {
+}
---
+++ app/src/main/java/com/example/carlydemo/carselection/CarSelectionScreen.kt
@@ -0,0 +1,7 @@
+package com.example.carlydemo.carselection
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CarSelectionScreen() {
+}
