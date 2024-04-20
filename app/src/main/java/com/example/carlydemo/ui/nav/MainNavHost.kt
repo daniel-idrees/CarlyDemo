860932de13355e55928eeabaf3bb37a662538647
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-20 03:21:10 +0200

    view model passed to the screen composable function

--- app/src/main/java/com/example/carlydemo/ui/nav/MainNavHost.kt
+++ app/src/main/java/com/example/carlydemo/ui/nav/MainNavHost.kt
@@ -18,6 +18,7 @@ fun MainNavHost() {
     ) {
         dashboardScreen {
             DashboardScreen(
+                viewModel = hiltViewModel(),
                 navigateToCarSelection = navController::navigateToCarSelection,
                 navigateToCarList = navController::navigateToCarList
             )
@@ -32,6 +33,7 @@ fun MainNavHost() {
 
         carListScreen {
             CarListScreen(
+                viewModel = hiltViewModel(),
                 navigateToCarSelection = navController::navigateToCarSelection,
                 goBack = navController::navigateUp
             )
