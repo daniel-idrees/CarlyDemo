9266142e76487a5fbdf5f1c73329793c3117a96a
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-20 03:20:04 +0200

    initial implementation with todo comment

--- app/src/main/java/com/example/carlydemo/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/carlydemo/ui/dashboard/DashboardViewModel.kt
@@ -2,9 +2,19 @@ package com.example.carlydemo.ui.dashboard
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor(): ViewModel()  {
+class DashboardViewModel @Inject constructor() : ViewModel() {
+    private val _viewState: MutableStateFlow<DashboardUiState> =
+        MutableStateFlow(DashboardUiState.NoCarSelectedState)
 
+    val viewState: StateFlow<DashboardUiState>
+        get() = _viewState
+
+    init {
+        //TODO check wether selected car is available
+    }
 }
