53edc140d2e6f84ee26a16fb5a4aa78b4eec22e4
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-20 02:55:28 +0200

    initial ui state and viewmodel for dashboard

---
+++ app/src/main/java/com/example/carlydemo/ui/dashboard/DashboardUiState.kt
@@ -0,0 +1,8 @@
+package com.example.carlydemo.ui.dashboard
+
+import com.example.carlydemo.domain.model.SelectedCar
+
+sealed interface DashboardUiState {
+    data object NoCarSelectedState : DashboardUiState
+    data class CarSelectedState(val car: SelectedCar): DashboardUiState
+}
