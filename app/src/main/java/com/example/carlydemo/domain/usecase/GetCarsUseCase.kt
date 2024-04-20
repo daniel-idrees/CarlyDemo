593de5c5f132571085298d5b2a95bd901d04c7bf
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-20 02:53:12 +0200

    use case function return type set as flow

--- app/src/main/java/com/example/carlydemo/domain/usecase/GetCarsUseCase.kt
+++ app/src/main/java/com/example/carlydemo/domain/usecase/GetCarsUseCase.kt
@@ -1,10 +1,11 @@
 package com.example.carlydemo.domain.usecase
 
 import com.example.carlydemo.domain.repository.CarRepository
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetCarsUseCase @Inject constructor(
     private val carRepository: CarRepository
 ) {
-    fun get() = carRepository.getCars()
+    suspend fun get() = flowOf(carRepository.getCars())
 }
