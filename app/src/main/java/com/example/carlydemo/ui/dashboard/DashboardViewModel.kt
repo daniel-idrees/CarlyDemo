1de4e7f04ca6fcabd7b69a103dd2e3f74ecd01c1
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-20 17:16:34 +0200

    viewstate handling with car selected data

--- app/src/main/java/com/example/carlydemo/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/carlydemo/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.carlydemo.ui.dashboard
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.carlydemo.domain.usecase.GetMainSelectedCarUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor() : ViewModel() {
+class DashboardViewModel @Inject constructor(
+    private val getMainSelectedCarUseCase: GetMainSelectedCarUseCase
+) : ViewModel() {
     private val _viewState: MutableStateFlow<DashboardUiState> =
         MutableStateFlow(DashboardUiState.NoCarSelectedState)
 
@@ -15,6 +25,15 @@ class DashboardViewModel @Inject constructor() : ViewModel() {
         get() = _viewState
 
     init {
-        //TODO check wether selected car is available
+        viewModelScope.launch {
+            getMainSelectedCarUseCase.get()
+                .flowOn(Dispatchers.IO)
+                .catch { DashboardUiState.NoCarSelectedState }
+                .map { car ->
+                    DashboardUiState.CarSelectedState(car)
+                }.collect { uiState ->
+                    _viewState.update { uiState }
+                }
+        }
     }
 }
