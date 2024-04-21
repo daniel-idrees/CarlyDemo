7b66d398202b8bf1b6bdba876601747fb3ad6e7f
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-21 16:02:48 +0200

    dependency bind used in the repository module

--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,25 +1,20 @@
 package com.example.data.di
 
-import androidx.room.Dao
-import com.example.data.database.dao.CarDao
 import com.example.data.repository.CarRepositoryImpl
-import com.example.data.repository.source.OpenCSVManager
 import com.example.domain.repository.CarRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal class RepositoryModule {
+internal interface RepositoryModule {
 
+    @Binds
     @Singleton
-    @Provides
     fun providesCarRepository(
-        openCSVManager: OpenCSVManager,
-        dao: CarDao
-    ): CarRepository = CarRepositoryImpl(openCSVManager, dao)
+        impl: CarRepositoryImpl
+    ): CarRepository
 }
