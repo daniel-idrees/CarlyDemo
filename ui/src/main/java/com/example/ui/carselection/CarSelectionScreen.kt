8d78d05c8a9edf1cc4f78bc6904dcba6c896f7db
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-21 22:49:51 +0200

    text color changed in the search text field

--- ui/src/main/java/com/example/ui/carselection/CarSelectionScreen.kt
+++ ui/src/main/java/com/example/ui/carselection/CarSelectionScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -116,6 +117,7 @@ private fun MainView(
                     value = text,
                     singleLine = true,
                     onValueChange = { text = it },
+                    textStyle = TextStyle(color = FontDark),
                     label = {
                         Text(
                             text = searchBarHint,
