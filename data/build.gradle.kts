44fe9d82114f1381cf09f43adbb6fb8a8fa8833d
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-21 15:27:39 +0200

    data module added

---
+++ data/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.jetbrainsKotlinKapt)
+    alias(libs.plugins.googleDagger)
+}
+
+android {
+    namespace = "com.example.data"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    implementation (libs.hilt.android)
+    //kapt(libs.hilt.android.compiler)
+    kapt(libs.androidx.hilt.hilt.compiler)
+    kapt(libs.hilt.compiler)
+
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.opencsv)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+
+    implementation(libs.gson)
+    testImplementation(libs.junit)
+}
