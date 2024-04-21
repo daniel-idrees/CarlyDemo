8ba39251e69f27353f0fae3e4c660eea7a01cee2
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-21 19:15:33 +0200

    view state handling in dashboard viewmodel modified

--- ui/src/main/java/com/example/ui/dashboard/DashboardViewModel.kt
+++ ui/src/main/java/com/example/ui/dashboard/DashboardViewModel.kt
@@ -2,42 +2,33 @@ package com.example.ui.dashboard
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.core.di.IoDispatcher
 import com.example.domain.usecase.GetMainSelectedCarUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
-    private val getMainSelectedCarUseCase: GetMainSelectedCarUseCase,
-    @IoDispatcher
-    private val ioDispatcher: CoroutineDispatcher
+    getMainSelectedCarUseCase: GetMainSelectedCarUseCase,
 ) : ViewModel() {
-    private val _viewState: MutableStateFlow<DashboardUiState> =
-        MutableStateFlow(DashboardUiState.Loading)
 
-    val viewState: StateFlow<DashboardUiState>
-        get() = _viewState
-
-    init {
-        _viewState.update { DashboardUiState.Loading }
-        viewModelScope.launch {
-            getMainSelectedCarUseCase.get()
-                .flowOn(ioDispatcher)
-                .catch {
-                    _viewState.update { DashboardUiState.NoCarSelectedState }
-                }.collect {
-                    it?.let { car ->
-                        _viewState.update { DashboardUiState.CarSelectedState(car) }
-                    } ?: _viewState.update { DashboardUiState.NoCarSelectedState }
-                }
-        }
-    }
+    val viewState: StateFlow<DashboardUiState> =
+        getMainSelectedCarUseCase.get()
+            .catch { DashboardUiState.NoCarSelectedState }
+            .distinctUntilChanged()
+            .filterNotNull()
+            .mapLatest { car ->
+                DashboardUiState.CarSelectedState(car)
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5.seconds.inWholeMilliseconds),
+                initialValue = DashboardUiState.NoCarSelectedState
+            )
 }
