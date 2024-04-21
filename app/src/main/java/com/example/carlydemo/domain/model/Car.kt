b8d0c54dde38c2a2fda4a5e1a973607c77657486
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-21 15:36:18 +0200

    files moved

--- app/src/main/java/com/example/carlydemo/domain/model/Car.kt
+++
@@ -1,9 +0,0 @@
-package com.example.carlydemo.domain.model
-
-data class Car(
-    val brand: String,
-    val series: String,
-    val minSupportedYear: Int,
-    val maxSupportedYear: Int,
-    val features: List<String>,
-)
