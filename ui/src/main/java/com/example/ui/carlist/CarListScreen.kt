b0a573b3214384cb588d4466d1e1befe2206b7f7
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-22 20:10:19 +0200

    gradient color for background set

--- ui/src/main/java/com/example/ui/carlist/CarListScreen.kt
+++ ui/src/main/java/com/example/ui/carlist/CarListScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -93,7 +94,14 @@ private fun MainView(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(color = BackgroundLight)
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            BackgroundLight,
+                            BackgroundDark
+                        )
+                    )
+                )
                 .padding(contentPadding)
         ) {
 
