1ae8dce75e8e19ba0c09b168e87d6428b61feb05
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-22 21:54:28 +0200

    font configuration

---
+++ ui/src/main/java/com/example/ui/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.ui.R
+
+val sfProDisplayFont = FontFamily(
+    Font(R.font.sf_pro_display_bold, FontWeight.Bold)
+)
+
+val sfProTextFont = FontFamily(
+    Font(R.font.sf_pro_text_medium, FontWeight.Medium),
+    Font(R.font.sf_pro_text_regular, FontWeight.Normal)
+)
