729636af8877da79a0f38bc772321081f45474cc
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-04-23 00:13:41 +0200

    Surface added on top of compose navigation host

--- ui/src/main/java/com/example/ui/CarlyDemoApp.kt
+++ ui/src/main/java/com/example/ui/CarlyDemoApp.kt
@@ -1,12 +1,21 @@
 package com.example.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.ui.nav.MainNavHost
 import com.example.ui.theme.CarlyDemoTheme
 
 @Composable
 fun CarlyDemoApp() {
     CarlyDemoTheme {
-        MainNavHost()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Transparent,
+        ) {
+            MainNavHost()
+        }
     }
 }
